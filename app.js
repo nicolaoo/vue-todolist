7e2dc64672b4de4e0817b4104363e7e95560d520
nicola00 <nicolajobs00@gmail.com>
2023-02-21 15:16:47 +0100

    FEAT add click button and funcion addTasks

--- app.js
+++ app.js
@@ -4,7 +4,31 @@ const { createApp } = Vue
 createApp({
     data(){
         return{
-            
+            tasks:[
+                {
+                    text: 'una stringa che indica il testo del todo',
+                    done: 'un booleano (true/false) che indica se il todo è stato fatto oppure no'
+                },
+                {
+                    text: 'una stringa che indica il testo del todo',
+                    done: 'un booleano (true/false) che indica se il todo è stato fatto oppure no'
+                },
+                {
+                    text: 'una stringa che indica il testo del todo',
+                    done: 'un booleano (true/false) che indica se il todo è stato fatto oppure no'
+                },
+            ],
+
+            taskUser: ''
         }
+    },
+
+    methods:{
+        addTask() {
+            console.log(this.tasks, this.taskUser)
+
+            this.tasks.push(this.taskUser)
+            this.taskskUser = ''
+        },
     },
 }).mount('#app')
